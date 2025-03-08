d8110c1b00af8d1aa651514ccde05d7c91e03478
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-03-08 22:50:48 +0700

    Add HomePage layout with bottom navigation and profile details

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,10 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sha/shared/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: whiteColor,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        child: BottomNavigationBar(
+          backgroundColor: whiteColor,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_history.png', width: 20),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_statistic.png', width: 20),
+              label: 'Statistic',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_reward.png', width: 20),
+              label: 'Reward',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: purpleColor,
+        child: Image.asset('assets/ic_plus-circle.png', width: 24),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [buildProfile(), buildWalletCard(), buildLevel()],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Akane', style: grayTextStyle.copyWith(fontSize: 16)),
+              const SizedBox(height: 2),
+              Text(
+                'Kurokawa',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/img_profile.png'),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Icon(Icons.check_circle, color: greenColor, size: 16),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard() {
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(top: 30),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          image: AssetImage('assets/img_card.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Akane Kurokawa',
+            style: whiteTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+          ),
+          const SizedBox(height: 28),
+          Text(
+            '**** **** **** 1937',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+              letterSpacing: 6,
+            ),
+          ),
+          const SizedBox(height: 21),
+          Text('Balance', style: whiteTextStyle.copyWith(fontSize: 14)),
+          Text(
+            '\$ 1,000,000',
+            style: whiteTextStyle.copyWith(fontSize: 24, fontWeight: semiBold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLevel() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.all(22),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: whiteColor,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Level 1',
+                style: blackTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: medium,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                '55% ',
+                style: greenTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              Text(
+                'of \$ 10M',
+                style: blackTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(55),
+            child: LinearProgressIndicator(
+              value: 0.55,
+              minHeight: 5,
+              backgroundColor: lightBackgroundColor,
+              valueColor: AlwaysStoppedAnimation(greenColor),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
