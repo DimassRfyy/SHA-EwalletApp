2bd1c99a5f25df5a08c44d38711e08b4944c1955
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-03-11 14:03:29 +0700

    Add MoreDialog to HomePage; implement dialog display for additional services

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -265,7 +265,12 @@ class HomePage extends StatelessWidget {
               HomeServiceItem(
                 imageUrl: 'assets/ic_more.png',
                 title: 'More',
-                onTap: () {},
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const MoreDialog(),
+                  );
+                },
               ),
             ],
           ),
@@ -423,6 +428,77 @@ class HomePage extends StatelessWidget {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MoreDialog extends StatelessWidget {
+  const MoreDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.zero,
+      alignment: Alignment.bottomCenter,
+      content: Container(
+        padding: const EdgeInsets.all(30),
+        height: 326,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: lightBackgroundColor,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Do More With Us',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(height: 13),
+            Wrap(
+              spacing: 35,
+              runSpacing: 25,
+              children: [
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_smartphone.png',
+                  title: 'Data',
+                  onTap: () {},
+                ),
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_water.png',
+                  title: 'Water',
+                  onTap: () {},
+                ),
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_stream.png',
+                  title: 'Stream',
+                  onTap: () {},
+                ),
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_movie.png',
+                  title: 'Movie',
+                  onTap: () {},
+                ),
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_food.png',
+                  title: 'Food',
+                  onTap: () {},
+                ),
+                HomeServiceItem(
+                  imageUrl: 'assets/ic_globe.png',
+                  title: 'Travel',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
